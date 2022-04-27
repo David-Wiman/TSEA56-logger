e9ba67d348542b6e31d8315d43694b45b878e930
Liv Kåreborn <livka967@student.liu.se>
2022-04-27 16:03:27 +0200

    fix logger for image_proc data

--- src/log.cpp
+++ src/log.cpp
@@ -7,27 +7,30 @@
 #include <stdexcept>
 #include <ctime>
 
-std::fstream Logger::logstream{};
-std::fstream Logger::img_proc_logstream{};
+using namespace std;
+
+fstream Logger::logstream{};
+fstream Logger::img_proc_logstream{};
 
 //bool om man vill logga i konstruktorn
 
 /* Create and open log file, log time */
 void Logger::init(bool log_img_proc) {
-	logstream.open("log/log.txt", std::ios::app);
+	logstream.open("log/log.txt", ios::app);
 	if (!logstream) {
-		throw std::runtime_error("Could not open file");
+		throw runtime_error("Could not open file");
 	}
 	time_t now = time(nullptr);
-	logstream << "\n" << std::put_time(localtime(&now), "%T") << " Log started " << std::endl;
+	logstream << "\n" << put_time(localtime(&now), "%T") << " Log started " << endl;
 
 	// Log img data
-	img_proc_logstream.open("log/img_proc_log.txt", std::ios::app);
+	img_proc_logstream.open("log/img_proc_log.csv", ios::app);
 	if (!img_proc_logstream) {
-		throw std::runtime_error("Could not open file");
+		throw runtime_error("Could not open file");
 	}
 	now = time(nullptr);
-	img_proc_logstream << "\n" << std::put_time(localtime(&now), "%T") << " Log started " << std::endl;
+	img_proc_logstream << "lat_pos , angle_l , angle_r , stop_dist" << endl;
+	// img_proc_logstream << "\n" << put_time(localtime(&now), "%T") << " Log started " << endl;
 }
 
 /* Close dsafthe log file upon destruction */
@@ -37,17 +40,17 @@ void Logger::close() {
 }
 
 /*  Write to log file */
-void Logger::log(int severity, std::string origin, std::string type, float value) {
-    log(severity, origin, type, std::to_string(value));
+void Logger::log(int severity, string origin, string type, float value) {
+    log(severity, origin, type, to_string(value));
 }
 
-void Logger::log(int severity, std::string origin, std::string type, std::string value) {
+void Logger::log(int severity, string origin, string type, string value) {
     time_t now = time(nullptr) ;
-    logstream << std::put_time(localtime(&now), "%T") << ", " << EnumStrings[severity] << ", " << origin << ", " << type << ": " << value << std::endl;
+    logstream << put_time(localtime(&now), "%T") << ", " << EnumStrings[severity] << ", " << origin << ", " << type << ": " << value << endl;
 }
 
 void Logger::log_img_data(image_proc_t data) {
 	time_t now = time(nullptr) ;
-	img_proc_logstream << std::put_time(localtime(&now), "%T") << ", " << data.lateral_position <<" : "<< data.angle_left 
-						<<" : "<< data.angle_right <<" : "<< data.stop_distance << std::endl;
+	img_proc_logstream << "  " << data.lateral_position <<"   ,    " << data.angle_left 
+						<<"    ,    " << data.angle_right <<"    ,    " << data.stop_distance << endl;
 }
