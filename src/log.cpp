1dfebc71e4707157c8e9f4594c8c5e707b271a1b
Liv Kåreborn <livka967@student.liu.se>
2022-05-03 15:27:14 +0200

    improve logger of image data

--- src/log.cpp
+++ src/log.cpp
@@ -29,7 +29,7 @@ void Logger::init(bool log_img_proc) {
 		throw runtime_error("Could not open file");
 	}
 	now = time(nullptr);
-	img_proc_logstream << "lat_pos , angle_l , angle_r , stop_dist" << endl;
+	img_proc_logstream << "time," << setw(10) << "status," << setw(11) << "lat_pos," << setw(11) << "angle_l," << setw(11) << "angle_r," << setw(12) << "stop_dist" << endl;
 	// img_proc_logstream << "\n" << put_time(localtime(&now), "%T") << " Log started " << endl;
 }
 
@@ -51,6 +51,6 @@ void Logger::log(int severity, string origin, string type, string value) {
 
 void Logger::log_img_data(image_proc_t data) {
 	time_t now = time(nullptr) ;
-	img_proc_logstream << "  " << data.lateral_position <<"   ,    " << data.angle_left 
-						<<"    ,    " << data.angle_right <<"    ,    " << data.stop_distance << endl;
+	img_proc_logstream << put_time(localtime(&now), "%T") <<"," << setw(3) << data.status_code <<"," << setw(9) << data.lateral_position << "," << setw(10) << data.angle_left 
+						<< "," << setw(10) << data.angle_right << "," << setw(11) << data.stop_distance << endl;
 }
