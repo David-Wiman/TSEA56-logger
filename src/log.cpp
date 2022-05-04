c51fda7ec1e2afa3e678dc58869be20e8ed8a4df
Albin Wahlén <albwa833@student.liu.se>
2022-05-04 11:09:47 +0200

    Remove tabs and trailing spaces

--- src/log.cpp
+++ src/log.cpp
@@ -16,27 +16,27 @@ fstream Logger::img_proc_logstream{};
 
 /* Create and open log file, log time */
 void Logger::init(bool log_img_proc) {
-	logstream.open("log/log.txt", ios::app);
-	if (!logstream) {
-		throw runtime_error("Could not open file");
-	}
-	time_t now = time(nullptr);
-	logstream << "\n" << put_time(localtime(&now), "%T") << " Log started " << endl;
+    logstream.open("log/log.txt", ios::app);
+    if (!logstream) {
+        throw runtime_error("Could not open file");
+    }
+    time_t now = time(nullptr);
+    logstream << "\n" << put_time(localtime(&now), "%T") << " Log started " << endl;
 
-	// Log img data
-	img_proc_logstream.open("log/img_proc_log.csv", ios::app);
-	if (!img_proc_logstream) {
-		throw runtime_error("Could not open file");
-	}
-	now = time(nullptr);
-	img_proc_logstream << "time," << setw(10) << "status," << setw(11) << "lat_pos," << setw(11) << "angle_l," << setw(11) << "angle_r," << setw(12) << "stop_dist" << endl;
-	// img_proc_logstream << "\n" << put_time(localtime(&now), "%T") << " Log started " << endl;
+    // Log img data
+    img_proc_logstream.open("log/img_proc_log.csv", ios::app);
+    if (!img_proc_logstream) {
+        throw runtime_error("Could not open file");
+    }
+    now = time(nullptr);
+    img_proc_logstream << "time," << setw(10) << "status," << setw(11) << "lat_pos," << setw(11) << "angle_l," << setw(11) << "angle_r," << setw(12) << "stop_dist" << endl;
+    // img_proc_logstream << "\n" << put_time(localtime(&now), "%T") << " Log started " << endl;
 }
 
 /* Close dsafthe log file upon destruction */
 void Logger::close() {
-	logstream.close();
-	img_proc_logstream.close();
+    logstream.close();
+    img_proc_logstream.close();
 }
 
 /*  Write to log file */
@@ -50,7 +50,7 @@ void Logger::log(int severity, string origin, string type, string value) {
 }
 
 void Logger::log_img_data(image_proc_t data) {
-	time_t now = time(nullptr) ;
-	img_proc_logstream << put_time(localtime(&now), "%T") <<"," << setw(3) << data.status_code <<"," << setw(9) << data.lateral_position << "," << setw(10) << data.angle_left 
-						<< "," << setw(10) << data.angle_right << "," << setw(11) << data.stop_distance << endl;
+    time_t now = time(nullptr) ;
+    img_proc_logstream << put_time(localtime(&now), "%T") <<"," << setw(3) << data.status_code <<"," << setw(9) << data.lateral_position << "," << setw(10) << data.angle_left
+                        << "," << setw(10) << data.angle_right << "," << setw(11) << data.stop_distance << endl;
 }
